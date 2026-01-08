212733a905ee928626e4a7c671120944cd9ad9d4
riodelphino <ryo.heavenlysky@gmail.com>
2026-01-09 04:47:45 +0900

    fix: Package.swift for SwiftPM

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
    name: "macime",
-   platforms: [.macOS(.v15)],
+   platforms: [.macOS(.v13)],
    products: [
       .executable(name: "macime", targets: ["macime"])
    ],
